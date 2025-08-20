2c826c0fbda1a76d5f195e8fb37a0e0d77fbe302
factor-4 <snk.sojib@gmail.com>
2025-08-20 18:25:56 +0300

    student dao implementation

---
+++ src/main/java/com/example/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,31 @@
+package com.example.cruddemo.dao;
+
+import com.example.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    //inject entity manager using constructor injector
+
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    //implement save method
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+
+    }
+}
